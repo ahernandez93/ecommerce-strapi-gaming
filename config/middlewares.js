850fef78631916a18154102a78fca01f71374b30
Allan <allan_hc@outlook.com>
2026-09-04 13:56:48 -0600

    Configuracion de bucket AWS

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,37 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+    "strapi::logger",
+    "strapi::errors",
+    "strapi::cors",
+    "strapi::poweredBy",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
+    {
+        name: "strapi::security",
+        config: {
+            contentSecurityPolicy: {
+                useDefaults: true,
+                directives: {
+                    "connect-src": ["'self'", "https:"],
+                    "img-src": [
+                        "'self'",
+                        "data:",
+                        "blob:",
+                        "market-assets.strapi.io",
+                        "ecommerce-strapi-ah.s3.us-east-1.amazonaws.com",
+                    ],
+                    "media-src": [
+                        "'self'",
+                        "data:",
+                        "blob:",
+                        "market-assets.strapi.io",
+                        "ecommerce-strapi-ah.s3.us-east-1.amazonaws.com",
+                    ],
+                    upgradeInsecureRequests: null,
+                },
+            },
+        },
+    },
 ];
--- config/plugins.js
+++ config/plugins.js
@@ -1,41 +1,56 @@
 const allowedMediaTypes = [
-  'image/*',
-  'video/*',
-  'audio/*',
-  'application/pdf',
-  'application/msword',
-  'application/vnd.openxmlformats-officedocument.*',
-  'text/plain',
-  'text/csv',
+    "image/*",
+    "video/*",
+    "audio/*",
+    "application/pdf",
+    "application/msword",
+    "application/vnd.openxmlformats-officedocument.*",
+    "text/plain",
+    "text/csv",
 ];
 
 const deniedTypes = [
-  'image/svg+xml',
-  'application/vnd.microsoft.portable-executable',
-  'application/x-msdownload',
-  'application/x-msdos-program',
-  'application/x-executable',
-  'application/x-dosexec',
-  'application/x-sh',
-  'text/x-shellscript',
-  'application/x-mach-binary',
+    "image/svg+xml",
+    "application/vnd.microsoft.portable-executable",
+    "application/x-msdownload",
+    "application/x-msdos-program",
+    "application/x-executable",
+    "application/x-dosexec",
+    "application/x-sh",
+    "text/x-shellscript",
+    "application/x-mach-binary",
 ];
 
-module.exports = () => ({
-  'users-permissions': {
-    config: {
-      jwtManagement: 'refresh',
-      sessions: {
-        httpOnly: true,
-      },
+module.exports = ({ env }) => ({
+    "users-permissions": {
+        config: {
+            jwtManagement: "refresh",
+            sessions: {
+                httpOnly: true,
+            },
+        },
     },
-  },
-  upload: {
-    config: {
-      security: {
-        allowedTypes: allowedMediaTypes,
-        deniedTypes,
-      },
+    upload: {
+        config: {
+            provider: "aws-s3",
+            providerOptions: {
+                s3Options: {
+                    credentials: {
+                        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+                        secretAccessKey: env("AWS_ACCESS_SECRET"),
+                    },
+                    region: env("AWS_REGION"),
+                    params: {
+                        ACL: env("AWS_ACL", "public-read"),
+                        Bucket: env("AWS_BUCKET"),
+                    },
+                },
+            },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
+            },
+        },
     },
-  },
 });
